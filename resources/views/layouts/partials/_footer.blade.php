6341667198ccdad0ba15be57cad32071e3a96ffe
Camara07 <Camara07@github.com>
2020-09-27 14:17:52 -0400

    first commit setup and add static pages

---
+++ resources/views/layouts/partials/_footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="text-center">
+    <p>&copy; {{date('Y')}} &middot; Laracaste by <a href="#">@dioux</a> - Laramap clone app</p>
+
+    <p><b>This app has been built for learning</b></p>
+</footer>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.default')
+@section('content')
+    <h1>Laracarte</h1>
+@endsection
