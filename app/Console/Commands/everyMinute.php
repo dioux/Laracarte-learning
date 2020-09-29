f5e6aacd920e18bc01b5d779520b0464a088c51e
Camara07 <Camara07@github.com>
2020-09-29 00:04:28 -0400

    add test schedule minute:update

---
+++ app/Console/Commands/everyMinute.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class everyMinute extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'minute:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Efface les de donnees de table test dans BD';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        DB::table('test')->delete();
+        echo "Operation effectuée avec succés";
+    }
+}
---
+++ resources/views/emails/message/created.blade.php
@@ -0,0 +1,6 @@
+<h1>{{$name}}</h1>
+<ul>
+    <li>{{$email}}</li>
+    <li>{{$msg}}</li>
+</ul>
+
